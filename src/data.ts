18188f5fa5a3e82cb51ba7f5fbe28a8d74535263
Lucas dos Santos Alves <30706052+lucasdsalves@users.noreply.github.com>
2023-11-14 18:03:06 -0300

    feat: react admin panel

---
+++ src/data.ts
@@ -0,0 +1,62 @@
+export const menu = [
+    {
+        id: 1,
+        title: "main",
+        listItems: [
+            {
+                id: 1,
+                title: "Homepage",
+                url: "/",
+                icon: "home.svg",
+            },
+            {
+                id: 2,
+                title: "Profile",
+                url: "/users/1",
+                icon: "user.svg",
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "lists",
+        listItems: [
+            {
+                id: 1,
+                title: "Users",
+                url: "/users",
+                icon: "user.svg",
+            },
+            {
+                id: 2,
+                title: "Products",
+                url: "/products",
+                icon: "product.svg",
+            }
+        ]
+    },
+]
+
+export const topDealUsers = [
+    {
+        id: 1,
+        img: "../noavatar.png",
+        username: "Top user",
+        email: "email@email.com",
+        amount: "15.000",
+    },
+    {
+        id: 2,
+        img: "../noavatar.png",
+        username: "Top second user",
+        email: "email@email.com",
+        amount: "10.000",
+    },
+    {
+        id: 3,
+        img: "../noavatar.png",
+        username: "Top third user",
+        email: "email@email.com",
+        amount: "8.000",
+    },
+]
